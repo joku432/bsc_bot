fff574024516b9d3da11e3466840cd68ef99e407
joku432 <poistajalol@hotmail.com>
2021-06-06 11:01:37 +0300

    successfully interacted with a contract

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,19 @@
 from web3 import Web3
 import json
+from .bot_config import *
 
 class Bot:
-    def __init__(self, contract: str):
-        self.contract = contract
-        self.web3 = Web3(Web3.HTTPProvider("https://bsc-dataseed.binance.org/"))
-    
-    def run() -> None:
+    def __init__(self, contract_addr: str):
+        self.contract_addr = Web3.toChecksumAddress(contract_addr)
+        self.web3 = Web3(Web3.HTTPProvider(DATASEED_ADDRESS))
+        self.contract = self.web3.eth.contract(address=self.contract_addr, abi=json.loads(ABI_STR))
+
+    def run(self) -> None:
         pass
+
+    def print_contract_total_supply(self) -> None:
+        wei = self.contract.functions.totalSupply().call()
+        print(Web3.fromWei(wei, 'ether'))
 
     def set_buy_limit(self, limit: float) -> None:
         pass
@@ -17,5 +23,6 @@ class Bot:
 
     def _check_liquidity_added(self) -> bool:
         pass
+
 
         
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from bot import Bot
 
 def main():
-    bot = Bot(contract="0x27ae27110350b98d564b9a3eed31baebc82d878d")
-    bot.run()
+    bot = Bot(contract_addr="0x27ae27110350b98d564b9a3eed31baebc82d878d")
+    bot.print_contract_total_supply()
+    #bot.run()
 
 if __name__ == "__main__":
     main()
