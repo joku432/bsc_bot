3ff7306f95bab7d112c0b0b0122880bf0d4a4eee
joku432 <poistajalol@hotmail.com>
2021-06-07 21:19:06 +0300

    settings class

--- bot/bot.py
+++ bot/bot.py
@@ -9,26 +9,31 @@ class Bot:
         self.contract_addr = Web3.toChecksumAddress(contract_addr)
         self.web3 = Web3(Web3.HTTPProvider(DATASEED_ADDRESS))
         self.contract = self.web3.eth.contract(address=self.contract_addr, abi=json.loads(ABI_STR))
-        load_dotenv()
-        self.secret_key = os.getenv("SECRET_KEY")
-        #if self.secret_key is None:
-        #    raise Exception()
-        
+        # My account's address
+        self.account = Web3.toChecksumAddress("0xD09c87E4701fa8955D21CC40EaE1D5A6CB1323e3")
+        self.settings = Settings()
 
-    def run(self) -> None:
-        pass
-            
-    def print_contract_total_supply(self) -> None:
+    def run(self):
+        start_balance = self._get_account_balance()
+        if start_balance <= 0.001:
+            print("Balance is too small.")
+            return
+
+    def _get_account_balance(self):
+        ether = self.web3.fromWei(self.web3.get_balance(self.account), "ether")
+        return ether
+
+    def print_contract_total_supply(self):
         wei = self.contract.functions.totalSupply().call()
         print(Web3.fromWei(wei, 'ether'))
 
-    def set_buy_limit(self, limit: float) -> None:
+    def set_buy_limit(self, limit: float):
+        self.settings
+
+    def set_sell_limit(self, limit: float):
         pass
 
-    def set_sell_limit(self, limit: float) -> None:
-        pass
-
-    def _check_liquidity_added(self) -> bool:
+    def _check_liquidity_added(self):
         pass
 
     def _buy(self, amount: float):
